8d5007e5046cc25bdff8faaee3dec3de236d85aa
yadavhimanshu059 <yadavhimanshu059@gmail.com>
2016-12-20 23:58:50 +0530

    Update conversion.py

--- conversion.py
+++ conversion.py
@@ -8,11 +8,11 @@ from os.path import isfile, join
 from Word_order import *
 from Clause_joining import *
 
-   
-ssf = SSF(sample_input.dat)
+
+ssf = SSF("sample_input.dat")
 for tree in ssf.getTrees():
     draw=Join_clauses(tree)
     PS_tree=draw.ps_tree()
-    print PS_tree
+    print PS_tree+'\n\n'
 
             
